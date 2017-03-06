c6e3ebed146c6655de0b02c6160b0dc834130d27
dinosaurleg <jaeshin5553@utexas.edu>
2017-03-05 22:47:46 -0600

    Got each individual pixel.

--- imageFuzzer.java
+++ imageFuzzer.java
@@ -9,6 +9,7 @@ public class imageFuzzer{
 		File f = null;
 		int height = 0;
 		int width = 0;
+		ArrayList pixelList<Integer> = new ArrayList<Integer>();
 
 		try{
 			image = getBufferedImage(image, f, height, width);
@@ -21,6 +22,8 @@ public class imageFuzzer{
 		}catch(IOException e){
 			System.out.println("Error: " + e);
 		}
+
+		getRegion(image, height, width);
 	}
 
 	public static BufferedImage getBufferedImage(BufferedImage image, File f, int height, int width) throws IOException{
@@ -30,6 +33,8 @@ public class imageFuzzer{
 				image = ImageIO.read(f);
 				height = (int)image.getHeight();
 				width = (int)image.getWidth();
+				System.out.println(height);
+				System.out.println(width);
 				image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 				image = ImageIO.read(f);
 				System.out.println("File finished uploading!");
@@ -58,6 +63,47 @@ public class imageFuzzer{
 			System.out.println("Error: " + e);
 			System.out.println("After error +++++++++++++++");
 		}
+	}
+
+	public static void getRegion(BufferedImage image, int height, int width, ArrayList<Integer> pixelList) throws IOException{
+		int column;
+		int row;
+		height = (int)image.getHeight();
+		width = (int)image.getWidth();
+		int[][] pixelArray = new int[width][height];
+		int a, r, g, b = 0;
+
+
+		System.out.println("hdalskdnlajsna");
+		System.out.println(height);
+		System.out.println(width);
+		for(int x = 0; x < width; x++){
+			for(int y = 0; y < height; y++){
+				pixelArray[x][y] = image.getRGB(x,y);
+			}
+		}
+
+		for(int w = 0; w < width; w++){
+			row = w;
+			for(int h = 0; h < height; h++){
+				column = h;
+
+				getRGBValues(getRGB(w, h), pixelList);
+				a = pixelList(0);
+				r = pixelList(1);
+				g = pixelList(2);
+				b = pixelList(3);
+			}
+		}
+	}
+
+	public static arrayList<int> getRGBValues(int pixel, pixelList){
+		pixelList.add((pixel>>24)&0xff);
+		pixelList.add((pixel>>16)&0xff);
+		pixelList.add((pixel>>8)&0xff);
+		pixelList.add(pixel&0xff);
+
+		return pixelList;
 	}
 
 }
